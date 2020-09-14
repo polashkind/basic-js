fcda75d415cc66f13853b85312ac13cfd92f763b
Polashkind <polashkind@gmail.com>
2020-09-14 15:42:20 +0300

    carbon-dating task done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+
+  return Number.isNaN(activity) || activity > 15 || activity <= 0
+    ? false 
+    : Math.ceil(HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / activity) / Math.LN2.toFixed(3));
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,8 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    const { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options;
+    const mainStr = Array(repeatTimes).fill(str).join(separator);
+    const additionalStr = Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+    
+    return mainStr + additionalStr;
 };
   
